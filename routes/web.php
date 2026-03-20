a511f4736fc79dad6e07e4cf5deadc9c35fa7175
KatySS369 <246110828+KatySS369@users.noreply.github.com>
2026-03-20 10:19:47 -0500

    Agregue ruta para el panel index de voluntarios

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VoluntarioController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,3 +12,4 @@ use App\Http\Controllers\AdoptanteController;
 Route::get('/index', [AdoptanteController::class, 'index'])->name('adoptante');
 Route::get('/quienes', [AdoptanteController::class, 'quienes']);
 Route::get('/adopta', [AdoptanteController::class, 'adopta']);
+Route::get('/indexvoluntario', [VoluntarioController::class, 'indexvoluntario'])->name('indexvoluntario'); 
